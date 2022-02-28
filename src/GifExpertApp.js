5412a809f286767999afc8f62d04a921597a54ce
Uriel <uriellopezaquino1.0@gmail.com>
2022-02-27 19:03:05 -0600

    Creando una lista de categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import react from 'react'
+import  react, {useState} from 'react'
 
 export const GifExpertApp = () => {
+
+    //const categories = [ 'One Punch Man', 'Dragon Ball', 'Sword Art Online'  ];
+    const [categories, setCategories] = useState ( [ 'One Punch Man', 'Dragon Ball', 'Sword Art Online'  ]);
+
+    const handleAdd = () => {
+            setCategories( cats => [...categories + 'Attak on Titan'] );
+    }
+
     return (
         <>
-            <h2>GofExpertApp</h2>
+            <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={ handleAdd }>agregar</button>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return<li key={category}>  {category} </li>
+                    })
+                }
+            </ol>
+
         </>
     )
 }
